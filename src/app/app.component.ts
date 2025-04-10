fa1284d08554cacb2e300c1ac1aac8bcd663ce58
Eric <eric941106@gmail.com>
2025-04-10 10:22:38 +0800

    feat(product): 顯示無產品資料訊息實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,46 +10,52 @@ import { Product } from './models/product';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  products = [
-    new Product({
-      name: 'A 產品',
-      authors: '作者 A、作者 B、作者 C',
-      company: '博碩文化',
-      isDiscounted: true,
-      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      price: 1580,
-    }),
-    new Product({
-      name: 'B 產品',
-      authors: '作者 A、作者 B、作者 C',
-      company: '博碩文化',
-      isDiscounted: true,
-      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      price: 1580,
-    }),
-    new Product({
-      name: 'C 產品',
-      authors: '作者 A、作者 B、作者 C',
-      company: '博碩文化',
-      isDiscounted: true,
-      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      price: 1580,
-    }),
-    new Product({
-      name: 'D 產品',
-      authors: '作者 A、作者 B、作者 C',
-      company: '博碩文化',
-      isDiscounted: true,
-      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      price: 1580,
-    }),
-    new Product({
-      name: 'E 產品',
-      authors: '作者 A、作者 B、作者 C',
-      company: '博碩文化',
-      isDiscounted: true,
-      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-      price: 1580,
-    }),
-  ];
+  products: Product[] = [];
+  setEmptyData(): void {
+    this.products = [];
+  }
+  setHasData(): void {
+    this.products = [
+      new Product({
+        name: 'A 產品',
+        authors: '作者 A、作者 B、作者 C',
+        company: '博碩文化',
+        isDiscounted: true,
+        photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+        price: 1580,
+      }),
+      new Product({
+        name: 'B 產品',
+        authors: '作者 A、作者 B、作者 C',
+        company: '博碩文化',
+        isDiscounted: false,
+        photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+        price: 1580,
+      }),
+      new Product({
+        name: 'C 產品',
+        authors: '作者 A、作者 B、作者 C',
+        company: '博碩文化',
+        isDiscounted: true,
+        photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+        price: 1580,
+      }),
+      new Product({
+        name: 'D 產品',
+        authors: '作者 A、作者 B、作者 C',
+        company: '博碩文化',
+        isDiscounted: false,
+        photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+        price: 1580,
+      }),
+      new Product({
+        name: 'E 產品',
+        authors: '作者 A、作者 B、作者 C',
+        company: '博碩文化',
+        isDiscounted: true,
+        photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+        price: 1580,
+      }),
+    ];
+  }
 }

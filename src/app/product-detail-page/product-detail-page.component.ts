19238e4b0659a0e2b1dc10c2f2e95acef053f04a
Eric <eric941106@gmail.com>
2025-05-01 01:11:57 +0800

    feat(product-detail): 產品檢視頁面切版實作

--- src/app/product-detail-page/product-detail-page.component.ts
+++ src/app/product-detail-page/product-detail-page.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Product } from '../models/product';
+import { CurrencyPipe } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-detail-page',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './product-detail-page.component.html',
-  styleUrl: './product-detail-page.component.scss'
+  styleUrl: './product-detail-page.component.scss',
 })
 export class ProductDetailPageComponent {
+  product = new Product({
+    name: 'I 產品',
+    authors: ['作者 A', '作者 B', '作者 C'],
+    company: '博碩文化',
+    isDiscounted: false,
+    photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+    price: 1580,
+  });
 
+  readonly router = inject(Router);
+
+  onBack(): void {
+    this.router.navigate(['products']);
+  }
+
+  onAddToCart(): void {
+    this.router.navigate(['products', 'cart']);
+  }
 }

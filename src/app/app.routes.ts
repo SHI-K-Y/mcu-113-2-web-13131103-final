411fbd4aa325df085ea42d7b40afc674d3075044
Eric <eric941106@gmail.com>
2025-05-07 22:56:14 +0800

    feat(resolver): 建立產品資料預載資料

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { CartPageComponent } from './cart-page/cart-page.component';
 import { ProductDetailPageComponent } from './product-detail-page/product-detail-page.component';
+import { productResolver } from './resolver/product.resolver';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'products' },
   { path: 'products', component: ProductPageComponent },
-  { path: 'product/view/:id', component: ProductDetailPageComponent },
+  { path: 'product/view/:id', component: ProductDetailPageComponent, resolve: { product: productResolver } },
   { path: 'cart', component: CartPageComponent },
 ];

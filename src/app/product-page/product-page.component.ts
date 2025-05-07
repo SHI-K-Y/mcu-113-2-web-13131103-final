09e210472804625033e3a8ef08c5a1e25b6d4fb4
Eric <eric941106@gmail.com>
2025-05-07 22:20:19 +0800

    refactor(product): 變更使用 to signal 轉換 observable

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,11 +1,12 @@
 import { ProductService } from './../services/product.service';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, computed, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Product } from '../models/product';
 import { ProductCardListComponent } from '../product-card-list/product-card-list.component';
 import { PaginationComponent } from '../pagination/pagination.component';
 import { FormsModule } from '@angular/forms';
 import { BehaviorSubject, combineLatest, startWith, Subject, switchMap, tap } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-product-page',
@@ -13,12 +14,13 @@ import { BehaviorSubject, combineLatest, startWith, Subject, switchMap, tap } fr
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.scss',
 })
-export class ProductPageComponent implements OnInit {
+export class ProductPageComponent {
   private router = inject(Router);
 
   private ProductService = inject(ProductService);
 
   private readonly pageIndex$ = new BehaviorSubject(1);
+
   get pageIndex() {
     return this.pageIndex$.value;
   }
@@ -30,26 +32,27 @@ export class ProductPageComponent implements OnInit {
 
   pageSize = 5;
 
-  totalCount = 0;
+  private readonly data$ = combineLatest([
+    this.pageIndex$.pipe(tap((value) => console.log('page index', value))),
+    this.refresh$.pipe(
+      startWith(undefined),
+      tap(() => console.log('refresh'))
+    ),
+  ]).pipe(switchMap(() => this.ProductService.getList(undefined, this.pageIndex, this.pageSize)));
 
-  products: Product[] = [];
+  private readonly data = toSignal(this.data$, { initialValue: { data: [], count: 0 } });
 
   searchName: string | undefined;
 
-  ngOnInit(): void {
-    combineLatest([
-      this.pageIndex$.pipe(tap((value) => console.log('page index', value))),
-      this.refresh$.pipe(
-        startWith(undefined),
-        tap(() => console.log('refresh'))
-      ),
-    ])
-      .pipe(switchMap(() => this.ProductService.getList(this.searchName, this.pageIndex, this.pageSize)))
-      .subscribe(({ data, count }) => {
-        this.products = data;
-        this.totalCount = count;
-      });
-  }
+  readonly totalCount = computed(() => {
+    const { count } = this.data();
+    return count;
+  });
+
+  readonly products = computed(() => {
+    const { data } = this.data();
+    return data;
+  });
 
   onAddToCart(product: Product): void {
     this.router.navigate(['product', 'cart', product.id]);

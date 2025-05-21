bd3e4fbf5cf6213fd3b6460999f684a1d70faaf0
Eric <eric941106@gmail.com>
2025-05-21 22:53:24 +0800

    feat(cart): 顧客表單姓名、地址、電話欄位設定、表單驗證設定

--- src/app/cart-page/cart-page.component.ts
+++ src/app/cart-page/cart-page.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-cart-page',
-  imports: [],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './cart-page.component.html',
-  styleUrl: './cart-page.component.scss'
+  styleUrl: './cart-page.component.scss',
 })
 export class CartPageComponent {
+  private readonly route = inject(ActivatedRoute);
 
+  form = new FormGroup({
+    name: new FormControl<string | null>(null, { validators: [Validators.required] }),
+    address: new FormControl<string | null>(null, { validators: [Validators.required] }),
+    tel: new FormControl<string | null>(null, { validators: [Validators.required] }),
+  });
+
+  get name(): FormControl<string | null> {
+    return this.form.get('name') as FormControl<string | null>;
+  }
+
+  get address(): FormControl<string | null> {
+    return this.form.get('address') as FormControl<string | null>;
+  }
+
+  get tel(): FormControl<string | null> {
+    return this.form.get('tel') as FormControl<string | null>;
+  }
 }
